46569ab676d49e9713454a5f437580a45abbb99f
Andrés Proaño <andres@oventi.org>
2017-06-08 23:00:26 +1000

    List method to fwo reports api

--- apps/fwo_reports_api/index.js
+++ apps/fwo_reports_api/index.js
@@ -5,7 +5,15 @@ const restify = require('restify')
 const FwoReportApi = require('./models/FwoReportApi.js')
 const redis = require('redis')
 
+//const json_documents_url = 'http://cdn.oventi.net/fwo_media_releases.test.json'
+const json_documents_url = 'http://cdn.oventi.net/fwo_augmented_media_releases.json'
+
+const request = require('request')
+const JSONStream = require('JSONStream')
+const es = require('event-stream')
+
 let server = restify.createServer({})
+server.use(restify.queryParser());
 server.use(restify.plugins.bodyParser())
 
 let redis_client = redis.createClient(6379, 'employer-watch.fovchw.0001.apse1.cache.amazonaws.com', {no_ready_check: true})
@@ -25,6 +33,18 @@ server.pre((req, res, next) => {
 })
 
 server.post('/:action', api.action)
+server.get('/list', api.list)
+
+// building report list
+let report_list = []
+request(json_documents_url)
+    .pipe(JSONStream.parse())
+    .pipe(es.mapSync(data => {
+        report_list.push(data)
+    }))
+    .on('end', () => {
+        api.set_reports(report_list)
+    })
 
 server.listen(PORT, () => {
     console.log('Employer Watch Fwo Reports API is running on port', PORT)
--- apps/fwo_reports_api/models/FwoReportApi.js
+++ apps/fwo_reports_api/models/FwoReportApi.js
@@ -4,8 +4,12 @@ const config = require('../config.js')
 
 class FwoReportApi {
     constructor(restify_server, redis_client) {
+        this.reports = []
+
         this.action = this.action.bind(this)
         this.on_request = this.on_request.bind(this)
+        this.set_reports = this.set_reports.bind(this)
+        this.list = this.list.bind(this)
 
         this.server = restify_server
         this.server.pre(this.on_request)
@@ -35,13 +39,31 @@ class FwoReportApi {
         let data = req.body
         let key = `${config.key_prefixes.reports.au}.${data.key}`
 
-        delete data['key']
+        //delete data['key']
         let json = JSON.stringify(data)
 
         console.log(`SET ${key} ${json}`)
         this.redis.set(key, json);
 
-        return {}
+        return { persisted: true }
+    }
+
+    set_reports(reports) {
+        this.reports = reports
+    }
+
+    list(req, res, next) {
+        console.log('FwoReportApi::list')
+
+        let page = parseInt(req.params.page.toLowerCase())
+        let page_size = parseInt(req.params.page_size.toLowerCase())
+
+        let begin = (page - 1) * page_size
+        let end = begin + page_size
+
+        res.send(200, this.reports.slice(begin, end))
+
+        next()
     }
 }
 
