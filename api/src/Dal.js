a65f00887f7d7c7736e43c99a5a1f7469c152c29
Andrés Proaño <andres@oventi.org>
2017-06-27 00:51:28 +1000

    Added nodemon and get all of an entity

--- api/src/Dal.js
+++ api/src/Dal.js
@@ -7,27 +7,39 @@ const daos = {
 class Dal {
     constructor(db) {
         this.db = db
+        this.models = {}
 
+        this.get_model = this.get_model.bind(this)
         this.get_all = this.get_all.bind(this)
     }
 
-    get_all(collection) {
-        console.log('H')
+    get_model(collection) {
         let dao_name = collection.substring(0, 1).toUpperCase() + collection.substring(1)
-        let dao = daos[dao_name] ? new daos[dao_name](this.db) : null
+        let dao = null, model = null
+
+        if(this.models[dao_name]) {
+            return this.models[dao_name]
+        }
+
+        if(daos[dao_name]) {
+            dao = new daos[dao_name](this.db)
+            model = this.db.model(dao_name, dao.get_schema())
+
+            this.models[dao_name] = model
+        }
+
+        return model
+    }
+
+    get_all(collection) {
+        let model = this.get_model(collection)
 
         return new Promise((resolve, reject) => {
-            console.log('I 1')
-            if(dao === null) {
+            if(model === null) {
                 // @TODO send error object
                 return reject()
             }
 
-            console.log('I 2')
-
-            console.log('get_all', dao)
-
-            let model = this.db.model(dao_name, dao.get_schema())
             model.find((err, data) => {
                 if (err) {
                     return reject(err)
